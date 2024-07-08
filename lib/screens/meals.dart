7e1bcf585b0ad39b247ad574b5ae3c4d33437e7e
widgetinfo <drastik.widget@gmail.com>
2024-07-08 10:19:21 +0530

    first commit

---
+++ lib/screens/meals.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:meal/screens/meal_details.dart';
+import '../models/meal.dart';
+import '../widget/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+  });
+
+  final String? title;
+  final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing  here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, i) => MealItem(
+          meal: meals[i],
+          onSelectMeal: (meal) {
+            selectMeal(context, meal);
+          },
+        ),
+      );
+    }
+
+    if (title == null) {
+      return content;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+      ),
+      body: content,
+    );
+  }
+}
---
+++ lib/temp.dart
@@ -0,0 +1,67 @@
+/// Prime Number
+
+// void main() {
+//   for (int i = 2; i <= 100; i++) {
+//     if (isPrime(i)) {
+//       print(i);
+//     }
+//   }
+// }
+//
+// bool isPrime(int num) {
+//   if (num <= 1) return false;
+//   for (int i = 2; i * i <= num; i++) {
+//     if (num % i == 0) return false;
+//   }
+//   return true;
+// }
+
+
+/// word ma s hoai to 'hello s' nahi to 'hello else'
+
+// void main() {
+//   String str = "shivam";
+//   bool count = false;
+//
+//   for (int i = 0; i < str.length; i++) {
+//     if (str[i] == 's' || str[i] == 'S') {
+//       count = true;
+//       break;
+//     }
+//   }
+//
+//   if (count) {
+//     print('Hello s');
+//   } else {
+//     print('Hello else');
+//   }
+// }
+
+
+/// eligibleToVote array
+
+// void main() {
+//   List<int> ages = [
+//     10,
+//     20,
+//     30,
+//     18,
+//     16,
+//     50,
+//     48,
+//   ];
+//
+//   List<int> eligibleToVote = [];
+//
+//   for (int age in ages) {
+//     if (age > 18) {
+//       eligibleToVote.add(age);
+//     }
+//   }
+//
+//   print(eligibleToVote);
+// }
+
+
+
+
